47fbdd8be3675b08bbea71fd07910f996bc88f69
samuelclerod <samuelclerod@gmail.com>
2018-12-14 12:11:19 -0300

    modificado entrypoint

--- entrypoint.rb
+++ entrypoint.rb
@@ -4,6 +4,8 @@ when 'server'
     system("bundle exec rails s -p 3000 -b '0.0.0.0'")
 when 'new'
     if system('rails new . --force --database=postgresql')
+        system('mv -rf config/database.yml config/database.yml')
+        system('cp container_files/database.yml config/database.yml')
         system('rails db:create')
     end
 end
