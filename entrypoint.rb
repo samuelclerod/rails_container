1ba284cc18e4b06fbf219db9fa274b66ecc63ada
samuelclerod <samuelclerod@gmail.com>
2018-12-17 10:55:16 -0300

    atlerado entrypoint

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,11 +1,12 @@
+#!/usr/bin/ruby
 
 case ARGV[0]
 when 'server'
     system("bundle exec rails s -p 3000 -b '0.0.0.0'")
 when 'new'
-    if system('rails new . --force --database=postgresql')
-        system('mv -rf config/database.yml config/database.yml')
+    if system('bundle exec rails new . --force --database=postgresql')
+        system('mv config/database.yml config/database_old.yml')
         system('cp container_files/database.yml config/database.yml')
-        system('rails db:create')
+        system('bundle exec rails db:create')
     end
 end
