4895e832ca52405c7fe91c6489e89e6a8daf21fc
samuelclerod <samuelclerod@gmail.com>
2018-12-18 11:58:20 -0300

    changed database.yml

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,15 +1,28 @@
 #!/usr/local/bin/ruby
+
+def create
+    system 'bundle exec rails new . --force --database=postgresql'
+    system 'mv ./config/database.yml ./config/database_old.yml' 
+    system 'cp ./container_files/database.yml ./config/' 
+    system 'bundle exec rails db:create' 
+end
+
+def server
+    port=3000
+    unless File.exists? 'db/seeds.rb'
+        create
+    end
+    system 'rm -rf ./tmp/pids/server.pid' 
+    system "bundle exec rails s -p #{port} -b 0.0.0.0" 
+end
+
 case ARGV[0]
 when 'ola'
     puts 'Olá Mundo!'
 when 'server'
-    system('rm -rf ./tmp/pids/server.pid')
-    system("bundle exec rails s -p 3000 -b 0.0.0.0")
+    server
 when 'create'
-    system('bundle exec rails new . --force --database=postgresql')
-    system('mv ./config/database.yml ./config/database_old.yml')
-    system('cp ./container_files/database.yml ./config/')
-    system('bundle exec rails db:create')
+    create
 else
     exec(ARGV.join(" "))
 end
